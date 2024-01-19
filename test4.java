6229cde2e850b2e85d313dfbdd6affe791698868
root <root@ip-10-0-0-143.ec2.internal>
2024-01-19 14:09:28 +0000

    added code for login

---
+++ test4.java
@@ -0,0 +1,9 @@
+// Code to login into web application
+public Class Login{
+
+	public static void main String ([] args){
+		WebDriverManager.ChromeDriver();
+		ChromeDriver driver= new ChromeDriver();
+		driver.get("www.facebok.com")
+	}
+}
