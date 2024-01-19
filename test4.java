5d3edc6e254ac30b8b303300f5b4fa13a856a198
manikumi <156924806+manikumi@users.noreply.github.com>
2024-01-19 19:43:14 +0530

    Updated login functionality code

--- test4.java
+++ test4.java
@@ -4,6 +4,10 @@ public Class Login{
 	public static void main String ([] args){
 		WebDriverManager.ChromeDriver();
 		ChromeDriver driver= new ChromeDriver();
-		driver.get("www.facebok.com")
+		driver.get("www.facebok.com");
+		driver.findElement(By.xpath(@[id='username'].sendKeys("manikumi");
+		driver.findElement(By.xpath(@[id='password'].sendKeys("Hello@123");
+		driver.findElement(By.xpath(@[id='submit'].click;
+		
 	}
 }
